10299feb5b0d9f7975d2dd78f952e8bc64f43b40
Okoro John Darlington <okorojohndarlington@gmail.com>
2022-12-07 15:27:41 +0100

    Commit

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "https://jonie-chatapp.onrender.com",
+	baseURL: "https://jonie-chatapp.onrender.com/api",
 });
 
 export default axiosInstance;

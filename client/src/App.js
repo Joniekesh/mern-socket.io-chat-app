61bf562caaa965403c4922d8d3b41c24ad4eb4af
Okoro John Darlington <okorojohndarlington@gmail.com>
2022-12-07 16:32:42 +0100

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		setSocket(io("https://jonie-chatapp.onrender.com/api"));
+		setSocket(io("https://jonie-chatapp.onrender.com/"));
 	}, []);
 
 	useEffect(() => {
